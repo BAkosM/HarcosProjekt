eb6b944977d72f00be7ebb3eb88780a9359b2f80
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-27 14:38:47 +0200

    Main methods add

---
+++ src/harcos/harcos.java
@@ -0,0 +1,62 @@
+package harcos;
+
+public class Harcos{
+    private String nev;
+    private int szint;
+    private int tapasztalat;
+    private int eletero;
+    private int alapEletero;
+    private int alapSebzes;
+
+    public Harcos(String nev, int statuszSablon) {
+    }
+    public String getNev(){
+        return nev;
+    }
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+    public int getSzint(){
+        return szint;
+    }
+    public void setSzint(int szint) {
+        this.szint = szint;
+    }
+    public int getTapasztalat(){
+        return tapasztalat;
+    }
+    public void setTapasztalat(int tapasztalat){
+        this.tapasztalat = tapasztalat;
+    }
+    public int getAlapEletero(){
+        return alapEletero;
+    }
+    public int getAlapSebzes(){
+        return alapSebzes;
+    }
+    public int getEletero(){
+        return eletero;
+    }
+    public void setEletero(int eletero){
+        this.eletero = eletero;
+    }
+    public int getSebzes(){
+        
+    }
+    public int getSzintLepeshez(){
+
+    }
+    public int getMaxEletero(){
+
+    }
+    public void megkuzd(){
+
+    }
+    public void gyogyul(){
+
+    }
+    @Override
+    public String toString() {
+        
+    }
+}
