468d7c3a6a4fdcb32a0620ffbe90599b239b34e2
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-27 19:48:51 +0200

    Add Menu

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 import harcos.*;
@@ -36,33 +37,64 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         beker();
         Scanner sr = new Scanner(System.in);
-        for(int i = 0; i < harcosok.size(); i++) {
-            System.out.println(harcosok.get(i));
-        }   
-        /*String menu = "";
-        while(i<1){
-            System.out.println("Mit szeterne csinálni a(licit 10%) b(licit x%) c(x festmény adatai) d(kilépés)");
+        String menu = "";
+        String playername = "";
+        String playerclass = "";
+        int j = 0;
+        int q = 0;
+        int b = 0;
+        int ellenfel = 0;
+        int kor = 3;
+        while(j<1){
+        System.out.print("Add meg a karaktered nevét: ");
+        playername = sr.nextLine();
+        System.out.print("Válasz karakter klaszt(1-Harcos,2-Íjász,3-Mágus):");
+        playerclass = sr.nextLine();
+        if(playerclass.equals("1")||playerclass.equals("2")||playerclass.equals("3")){
+            j++;
+        } else {
+            System.out.println("Hibás karakter név vagy klasz! Próbáld újra!");
+        }}
+        Harcos player = new Harcos(playername,playerclass);
+
+        while(q<1){
+            System.out.println("Ellenfelek: ");  
+            for(int i = 0; i < harcosok.size(); i++) {
+                System.out.println(harcosok.get(i));
+            }
+            while(b<1){
+            System.out.printf("%d kor az ellenség támadásáig",kor);   
+            System.out.println();
+            if(kor == 0){
+                kor = 3;
+                ellenfel = (int)(Math.random()*(harcosok.size()-1))+1;
+                harcosok.get(ellenfel).megkuzd(player);
+                System.out.printf("%s megtámadott!",harcosok.get(ellenfel).getNev());
+            }
+            System.out.println("Mit szeretnél csinálni (a-harcolok,b-gyógyulás,c-státusz,d-kilépés): ");
             menu = sr.next();
-            if(menu.equals("d")){
-                i++;
-                System.out.print("Kilépés...");
-            }else if(menu.equals("a")){
-                System.out.print("Hanyadik festményre:");
-                hanyadik = sr.nextInt();
-                festmenyek.get(hanyadik).licit();
+            if(menu.equals("a")){
+                System.out.println("Hanyadik ellenféllel küzdenél meg: ");
+                ellenfel = sr.nextInt();
+                if(ellenfel>harcosok.size()){
+                    System.out.println("Nincs ilyen ellenfél!");
+                }else{
+                    b++;
+                    player.megkuzd(harcosok.get(ellenfel));
+                }
             }else if(menu.equals("b")){
-                System.out.print("Hanyadik festményre:");
-                hanyadik = sr.nextInt();
-                System.out.print("Menyivel:");
-                mennyi = sr.nextInt();
-                festmenyek.get(hanyadik).licitmeret(mennyi);
+                b++;
+                player.gyogyul();
+            }else if(menu.equals("d")){
+                b++;
+                q++;
             }else if(menu.equals("c")){
-                System.out.print("Hanyadik festmény:");
-                hanyadik = sr.nextInt();
-                System.out.println(festmenyek.get(hanyadik).toString());
+                System.out.println(player);
             }else{
-                System.out.println("Hibaaaaaaaaaaaaaaaaaaaaaaaa! Próbáld újra!");
-            }           
-        }*/
+             System.out.println("Nincs ilyen opció!");   
+            }}
+            kor--;
+            b = 0;
+        }
     }
 }        
